2879533f699e8a68ab64e5577283b0bed4470021
TranserZ <mingjo20@skku.edu>
2026-01-30 01:19:13 +0900

    comments added.

--- src/kernel.c
+++ src/kernel.c
@@ -9,3 +9,8 @@ void kernel_main(void)
 		uart_send(uart_recv());
 	}
 }
+
+/*
+	Mini UART 장치를 이용하여 screen에 "Hello, world!" 문자열을 출력하고,
+	이후에는 infinite loop에 진입하여 Mini UART로부터 수신된 문자를 다시 송신하는 프로그램.
+*/
